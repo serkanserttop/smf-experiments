4f3340e34f46a2bf91057cabcdaa344a4b2e404c
Serkan Serttop <serkan.serttop@smartface.io>
2015-02-25 15:41:07 +0000

    primitive require.js idea

---
+++ smf/libs/requre.js
@@ -0,0 +1,24 @@
+function require(){
+	var scripts = {};
+
+	function attach(route, callback){
+		scripts[route] = callback;
+	}
+	function execute(route){
+		var callback = scripts[route];
+		if(typeof callback === 'function'){
+			callback();
+		}
+	}
+	function getKeys(){
+		return Object.keys(scripts);
+	}
+	
+	return {
+		"attach": attach,
+		"execute": execute,
+		"getKeys": getKeys
+	};
+	
+}
+require = require();
