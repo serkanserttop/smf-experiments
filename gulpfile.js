973a739ce45a458831cf43be4a6423e538ffacf0
Serkan Serttop <serkanserttop@yahoo.com>
2015-03-17 13:28:32 +0200

    update missing in the last commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var order = require('gulp-order');
 var to5 = require('gulp-6to5');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
 
 var paths = {
   scripts: [
@@ -47,5 +49,43 @@ gulp.task('es6', function () {
 gulp.task('watch-es6', function() {
   gulp.watch(['experiment/es6/es6.js'], ['es6']);
 });
+
+gulp.task('rename-pages', function() {
+  gulp.src("smf/pages/**/*.js", { base: '.' })
+    .pipe(rename(function (path) {
+      var dirname = path.dirname.replace('smf/pages', '');
+      var basename = dirname + '/' + path.basename;
+      if(basename[0] === '/'){
+        basename = basename.slice(1);
+      }
+      basename = basename.replace(/\//g, '.');
+      path.dirname = '.';
+      path.basename = 'Pages.' + basename;
+    }))
+    .pipe(gulp.dest("./Scripts"));
+
+  gulp.src("smf/app/*.js", { base: '.' })
+    .pipe(rename(function (path) {
+      path.dirname = '.';
+    }))
+    .pipe(gulp.dest("./Scripts"));
+});
+
+gulp.task('replace', function() {
+  return gulp.src('Scripts/Pages.*.js', { base : './' } )
+    .pipe(replace(/App\.router\.define\(\'(.*)\'.*/, function(match, replacement){
+      replacement = replacement.replace(/\//g, '.').replace('pages.', '', 'i');
+      return '(function(){\n\tvar pageName = \'' + replacement + '\', page = Pages[pageName] = new SMF.UI.Page();'
+    }))
+    .pipe(replace(/^\}\)\;/m, '})();'))
+    .pipe(replace(/\'pages\/.*\'/, function(match){
+      match = match.replace(/\//g, '.').replace('pages.', '', 'i');
+      return match;
+    }))
+    .pipe(replace('navgen = App.router.navigateGenerator', 'navgen = App.helpers.pageShow'))
+    .pipe(gulp.dest('./'));
+});
+
+
 
 gulp.task('default', ['watch']);
